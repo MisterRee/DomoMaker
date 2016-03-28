b81457e2c553d41525df5523d62dfcae2e5e12eb
Danny Lee <dxl1720@rit.edu>
2016-03-28 17:41:04 -0400

    First Heroku Commit

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ var redisPASS;
 
 if(process.env.REDISCLOUD_URL){
 	redisURL = url.parse(process.env.REDISCLOUD_URL);
-	redisPASS = redisIRL.auth.split(":")[1];
+	redisPASS = redisURL.auth.split(":")[1];
 }
 
 var router = require('./router.js');
